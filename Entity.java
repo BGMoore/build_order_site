36f48ce077a081845b3c380f059a906776943ffd
BGMoore <mooreg@smu.edu>
2021-04-20 15:23:45 -0500

    first commit from IntelliJ

---
+++ Entity.java
@@ -0,0 +1,13 @@
+public class Entity {
+
+    private supply;
+    private timestamp;
+    private int [] entityID;
+    private int [] quantity;
+
+    public Entity () {}
+
+
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # build_order_site
 Web-app which densely stores StarCraft 2 build orders and allows searching, filtering, and display.
+Runs on Java 16.0.1
