162f841cdff12378063c54b8f4b5698903f4ecdd
BGMoore <BGMoore>
2021-04-28 12:50:49 -0500

    basic entity and build order classes implemented, starting structure for conversion functions class

---
+++ Conversion.java
@@ -0,0 +1,10 @@
+public class Conversion {
+
+    //converts EntityIDs into a String of the corresponding Unit name
+    public String getEntityString(int EntityID) {
+        String s;
+        //implement ifs for finding name of entity here
+        return s;
+    }
+
+}
--- Entity.java
+++ Entity.java
@@ -1,13 +1,41 @@
+import java.lang.String;
 public class Entity {
 
-    private supply;
-    private timestamp;
-    private int [] entityID;
-    private int [] quantity;
+    //logo quiz
+    //algo/sort display
 
-    public Entity () {}
+    //class variables
+    private int supply;
+    private String timestamp;
+    private int entity_id;
+    private int quantity;
 
+    //default constructor and one with all variables
+    public Entity () {
+        this.supply = 12;
+        this.timestamp = "00:00";
+        this.entity_id = 0;
+        this.quantity = 1;
+    }
 
+    public Entity (int s, String ts, int id,int q) {
+        this.supply = s;
+        this.timestamp = ts;
+        this.entity_id = id;
+        this.quantity = q;
+    }
 
+    //public facing class variable access (getters)
+    public int getSupply() { return supply; }
 
+    public String getTimestamp() { return timestamp; }
+
+    public int getEntityID() { return entity_id; }
+
+    public int getQuantity() { return quantity; }
+
+    public String consolePrint() {
+        Conversion c = new Conversion();
+        return supply + " " + timestamp + " " + c.getEntityString(entity_id) + " x" + quantity + "\n";
+    }
 }
